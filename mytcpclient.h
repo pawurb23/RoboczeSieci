e1e490d01d4d63be5b15e3a784d25a13eaf2bc8e
KrzysztofRostek <kr316302@student.polsl.pl>
2026-04-21 23:46:13 +0200

    Update mytcpclient.h

--- mytcpclient.h
+++ mytcpclient.h
@@ -11,26 +11,23 @@ class myTCPclient : public QObject
 public:
     explicit myTCPclient(QObject *parent = nullptr);
     ~myTCPclient();
-
     void polaczZSerwerem(const QString &ip, quint16 port);
     void rozlaczZSerwerem();
     void wyslijDane(quint8 typ, const QByteArray &dane);
-
+    bool isConnected() const;
 signals:
     void polaczono();
     void rozlaczono();
     void odebranoDane(quint8 typ, QByteArray dane);
     void wystapilBlad(QString opisBledu);
-
 private slots:
     void czytajDaneZGniazda();
     void obsluzPolaczenie();
     void obsluzRozlaczenie();
-
 private:
     QTcpSocket *gniazdo;
     QByteArray bufor;
     const quint32 NAGLOWEK_SYNCHRONIZACJI = 0xABCD1234;
 };
 
-#endif
+#endif // MYTCPCLIENT_H
